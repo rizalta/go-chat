3f4fc16bea54ed37b4ecea94d6656b4aa3a9104c
rizalta <rizalta00@gmail.com>
2024-11-03 01:45:05 +0530

    Some changes to configurations. started working on pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import(NewType).Config} */
-module.exports = {
-  content: ["./cmd/web/**/*.html", "./cmd/web/**/*.templ"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-};
+/** @type import('tailwindcss').Config;*/
+export default {
+	content: ["./cmd/web/**/*.html", "./cmd/web/**/*.templ"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ["luxury"]
+	}
+}
