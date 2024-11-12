9205671e905d3cf1ac5e03f2213da218f5cce43a
rizalta <rizalta00@gmail.com>
2024-11-12 17:20:24 +0530

    initial works on paging done

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,10 +3,13 @@ package server
 import (
 	"context"
 	"go-chat/cmd/web"
+	"go-chat/cmd/web/components"
 	"go-chat/cmd/web/pages"
 	"go-chat/internal/handler"
 	"go-chat/internal/middlewares"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -33,6 +36,8 @@ func (s *Server) RegisterRoutes(ctx context.Context) http.Handler {
 
 	r.Get("/ws", wsHandler.HandleWS(ctx))
 
+	r.Get("/messages/{page}", s.messages(ctx))
+
 	return r
 }
 
@@ -46,5 +51,23 @@ func (s *Server) serveIndex(w http.ResponseWriter, r *http.Request) {
 	err := pages.Index(userID, messages).Render(r.Context(), w)
 	if err != nil {
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+	}
+}
+
+func (s *Server) messages(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("HX-Request") != "true" {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+		page, _ := strconv.Atoi(r.PathValue("page"))
+		messages, _ := s.messageRepo.GetMessages(ctx, int64(page))
+		var mm strings.Builder
+		for _, message := range messages {
+			var m strings.Builder
+			components.Message(*message, true).Render(ctx, &m)
+			mm.WriteString(m.String())
+		}
+		w.Write([]byte(mm.String()))
 	}
 }
